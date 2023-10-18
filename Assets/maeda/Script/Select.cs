74cc5023ae0faa566a03095ef683fcf0ed7f70c6
maeda-sa <s.maeda.sys21@morijyobi.ac.jp>
2023-10-18 12:38:28 +0900

    keika

---
+++ Assets/maeda/Script/Select.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Select : MonoBehaviour
+{
+    [SerializeField] private GameObject bike;
+    private bool isStop = false;
+
+    private void Start()
+    {
+        bike.transform.Rotate(new Vector3(0, 0, 0)); 
+    }
+
+    private void Update()
+    {
+        bike.transform.Rotate(new Vector3(0, 10, 0) * Time.deltaTime);
+    }
+
+
+    public void Option(GameObject item)
+    {
+        if(!isStop)
+        {
+            item.SetActive(true);
+            isStop = true;
+        }
+    }
+
+    public void Back(GameObject item)
+    {
+        if (isStop)
+        {
+            item.SetActive(false);
+            isStop = false;
+        }
+    }
+
+    public void EndGame()
+    {
+        if(!isStop)
+            Application.Quit();
+    }
+}
