041a4ec0678dee184854ef36f64a5074fd683a87
鶴田 竜馬 <r.tsuruta.sys21@morijyobi.ac.jp>
2023-12-27 18:51:28 +0900

    ボタン適応

--- Assets/maeda/Script/GameOver.cs
+++ Assets/maeda/Script/GameOver.cs
@@ -2,20 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Asset.maeda
 {
     public class GameOver : MonoBehaviour
     {
         private GameState _gs;
+        private int _count = 0;
+
         [SerializeField]TextMeshProUGUI guilt;
+        [SerializeField] private List<GameObject> _button;
+        [SerializeField] private PlayerInput _player;
+        private InputAction _input_right;
+        private InputAction _input_left;
+        private InputAction _input_check;
 
         private void Start()
         {
             _gs = GameObject.Find("GameState").GetComponent<GameState>();
             guilt.text = "Ç®ëOÇÃçﬂÇêîÇ¶ÇÎ";
 
+            _input_right = _player.actions["Right"];
+            _input_left = _player.actions["Left"];
+            _input_check = _player.actions["Check"];
+        }
+
+        private void Update()
+        {
+            for (int i = 0; i < _button.Count; i++)
+            {
+                if (i == _count) _button[i].GetComponent<Image>().enabled = false;
+                else _button[i].GetComponent<Image>().enabled = true;
+            }
+
+            if (_input_right.WasPressedThisFrame())
+            {
+                if (_count < _button.Count - 1) _count++;
+            }
+
+            if (_input_left.WasPressedThisFrame())
+            {
+                if (_count > 0) _count--;
+            }
+
+            if (_input_check.WasPressedThisFrame())
+            {
+                switch (_count)
+                {
+                    case 0:
+                        SceneChange("Select");
+                        break;
+                    case 1:
+                        SceneChange("");
+                        break;
+                }
+            }
+        }
+
+        public void Count(int c)
+        {
+            _count = c;
         }
 
         public  void SceneChange(string SceneName)
